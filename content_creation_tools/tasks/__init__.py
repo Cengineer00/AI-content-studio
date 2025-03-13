82b84eadc0047b0fb6ceb43a8540f7d1f7eb6922
Cengineer00 <kose.orhun@metu.edu.tr>
2025-03-13 16:21:19 +0300

    Create StableScrollTask

--- content_creation_tools/tasks/__init__.py
+++ content_creation_tools/tasks/__init__.py
@@ -2,3 +2,4 @@ from .text_to_image import TextToImageTask
 from .text_to_video import TextToVideoTask
 from .image_to_video import ImageToVideoTask
 from .type_flow import TypeFlowTask
+from .stable_scroll import StableScrollTask
