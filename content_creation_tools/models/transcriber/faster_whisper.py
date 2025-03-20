72260c0336c6a5f9197b64d97c65b38932cae063
Cengineer00 <kose.orhun@metu.edu.tr>
2025-03-20 12:03:48 +0300

    Conert FasterWhisper to BaseModel structure

--- content_creation_tools/models/transcriber/faster_whisper.py
+++ content_creation_tools/models/transcriber/faster_whisper.py
@@ -1,12 +1,63 @@
 from faster_whisper import WhisperModel
+from content_creation_tools.core import BaseModel
+from typing import Dict, Any, BinaryIO, Union, List, Tuple
+from numpy import ndarray
 
-model_size = "medium"
-model = WhisperModel(model_size)
+class FasterWhisper(BaseModel):
+    def __init__(self):
+        super().__init__("FasterWhisper Transcriber")
 
-audiofilename = "shorts_deneme.mp3"
+        self.model = None
+        
+        self.parameter_schema = {
+            'model_size_or_path': {
+                'type': str,
+                'description': 'Size of the model to use, a path to a converted model directory, or a CTranslate2-converted Whisper model ID from the HF Hub.',
+                'default': 'medium'
+            },
+            'device': {
+                'type': str,
+                'description': 'Device to use for computation ("cpu", "cuda", "auto").',
+                'default': 'auto'
+            },
+            'language': {
+                'type': str,
+                'description': 'The language spoken in the audio such as "en" or "fr". If not set, the language will be detected in the first 30 seconds of audio.',
+                'default': 'tr'
+            },
+            'log_progress': {
+                'type': bool,
+                'description': 'Whether to log progress to the console.',
+                'default': False
+            },
+            'max_initial_timestamp': {
+                'type': float,
+                'description': 'The initial timestamp cannot be later than this.',
+                'default': 10
+            }
+        }
 
-segments, info = model.transcribe(audiofilename, word_timestamps=True)
-segments = list(segments)  # The transcription will actually run here.
-for segment in segments:
-    for word in segment.words:
-        print("[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word))
+    def get_input(self) -> str:
+        audio = input("Path to the input file: ")
+        return audio
+    
+    def generate(self, params: Dict[str, Any], audio: Union[str, BinaryIO, ndarray]) -> List[Tuple[float, float, str]]:
+        if self.model is None:
+            print("\t╭─ Initializing the model...")
+            self.model = WhisperModel(params['model_size_or_path'], device=params['device'])
+
+        segments, info = self.model.transcribe(
+            audio=audio,
+            word_timestamps=True,
+            language=params['language'],
+            log_progress=params['log_progress'],
+            max_initial_timestamp=params['max_initial_timestamp']
+        )
+        segments = list(segments)  # The transcription will actually run here.
+
+        subtitles = []
+        for segment in segments:
+            for word in segment.words:
+                subtitles.append((word.start, word.end, word.word))
+
+        return subtitles
