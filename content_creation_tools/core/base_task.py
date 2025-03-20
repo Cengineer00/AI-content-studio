b3e48424585add3694f8259ac2ced0242fb04a9b
Cengineer00 <kose.orhun@metu.edu.tr>
2025-03-20 18:06:07 +0300

    Create BaseTask class

---
+++ content_creation_tools/core/base_task.py
@@ -0,0 +1,19 @@
+from typing import Dict, Any
+from abc import ABC, abstractmethod
+
+class BaseTask(ABC):
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    @abstractmethod
+    def execute(self, params: Dict[str, Any]):
+        """Abstract method to be implemented by subclasses"""
+        pass
+
+    @abstractmethod
+    def apply_result(self, result, timestamp):
+        """Abstract method to be implemented by subclasses"""
+        pass
